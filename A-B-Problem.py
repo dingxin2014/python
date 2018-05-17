ac870008e93c058bb7009aa70c8da6efcefd70f9
dingxin <dingxin@meituan.com>
2018-05-17 16:12:50 +0800

    alg

---
+++ A-B-Problem.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    @param a: An integer
+    @param b: An integer
+    @return: The sum of a and b
+    """
+    def aplusb(self, a, b):
+        # write your code here
+        while b != 0:
+            a, b = (a ^ b) & 0xffffffff, (a & b) << 1
+        return a
+
+
+if __name__ == '__main__':
+    s = Solution()
+    a = 200
+    b = -190
+    print(s.aplusb(a,b))
+
+
